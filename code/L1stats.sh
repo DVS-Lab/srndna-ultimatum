3e5e26dcde8c123c8b92a34114f65156525fd840
David V. Smith <david.v.smith@temple.edu>
2021-08-09 23:28:25 -0400

    typo in srndna-data path

--- code/L1stats.sh
+++ code/L1stats.sh
@@ -12,7 +12,7 @@
 # ensure paths are correct irrespective from where user runs the script
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
-srndnadatadir=/data/project/srndna-data #need to fix this upon release
+srndnadatadir=/data/projects/srndna-data #need to fix this upon release
 
 # study-specific inputs
 TASK=ultimatum
