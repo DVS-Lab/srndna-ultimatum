5d76162664daec8fa1f6c3ffe77e00bb249d916a
David V. Smith <david.v.smith@temple.edu>
2021-08-11 21:55:58 -0400

    rewrite and add more comments

--- code/L2stats.sh
+++ code/L2stats.sh
@@ -5,106 +5,57 @@ scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
 
 # setting inputs and common variables
-sm=6
 sub=$1
-nruns=$2
-type=$3
+type=$2
+task=ultimatum # edit if necessary
+sm=6 # edit if necessary
 MAINOUTPUT=${maindir}/derivatives/fsl/sub-${sub}
 
-# exceptions and conditionals for the task; need to exclude bad/missing runs
-if [ $sub -eq 145 ] || [ $sub -eq 152 ]; then # bad data
-	echo "skipping sub-${sub} for task-trust"
+
+# --- EDIT HERE start: exceptions and conditionals for the task
+
+if [ $sub -eq 120 ] || [ $sub -eq 145 ] || [ $sub -eq 131 ]; then # bad data
+	echo "skipping sub-${sub} for task-${task}"
+	exit
+fi
+NCOPES=10
+
+# ppi has more contrasts than act (phys), so need a different L2 template
+if [ "${type}" == "act" ]; then
+	ITEMPLATE=${maindir}/templates/L2_task-${task}_model-02_type-act.fsf
+	NCOPES=${NCOPES}
 else
-	if [ $sub -eq 129 ] || [ $sub -eq 138 ]; then # bad data
-		nruns=2
-		INPUT1=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-01_sm-${sm}.feat
-		INPUT2=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-02_sm-${sm}.feat
-	elif [ $sub -eq 118 ]; then # bad data
-		nruns=4
-		INPUT1=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-01_sm-${sm}.feat
-		INPUT2=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-02_sm-${sm}.feat
-		INPUT3=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-04_sm-${sm}.feat
-		INPUT4=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-05_sm-${sm}.feat
-	elif [ $sub -eq 111 ] || [ $sub -eq 128 ]; then # sub-111 (misses), sub-128 (bad registration)
-		nruns=4
-		INPUT1=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-02_sm-${sm}.feat
-		INPUT2=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-03_sm-${sm}.feat
-		INPUT3=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-04_sm-${sm}.feat
-		INPUT4=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-05_sm-${sm}.feat
-	elif [ $sub -eq 150 ]; then # bad data
-		nruns=4
-		INPUT1=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-01_sm-${sm}.feat
-		INPUT2=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-03_sm-${sm}.feat
-		INPUT3=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-04_sm-${sm}.feat
-		INPUT4=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-05_sm-${sm}.feat
-	elif [ $sub -eq 131 ]; then # bad data
-		nruns=2
-		INPUT1=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-01_sm-${sm}.feat
-		INPUT2=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-04_sm-${sm}.feat
-	else
-		INPUT1=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-01_sm-${sm}.feat
-		INPUT2=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-02_sm-${sm}.feat
-		INPUT3=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-03_sm-${sm}.feat
-		INPUT4=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-04_sm-${sm}.feat
-		INPUT5=${MAINOUTPUT}/L1_task-trust_model-01_type-${type}_run-05_sm-${sm}.feat
-	fi
+	ITEMPLATE=${maindir}/templates/L2_task-${task}_model-02_type-ppi.fsf
+	let NCOPES=${NCOPES}+1 # add 1 since we tend to only have one extra contrast for PPI
+fi
+INPUT1=${MAINOUTPUT}/L1_task-${task}_model-02_type-${type}_run-01_sm-${sm}.feat
+INPUT2=${MAINOUTPUT}/L1_task-${task}_model-02_type-${type}_run-02_sm-${sm}.feat
 
-	# check for existing output and re-do if missing/incomplete
-	OUTPUT=${MAINOUTPUT}/L2_task-trust_model-01_type-${type}_sm-${sm}
-	if [ -e ${OUTPUT}.gfeat/cope18.feat/cluster_mask_zstat1.nii.gz ]; then # check last (act) or penultimate (ppi) cope
-		echo "skipping existing output"
-	else
-		echo "re-doing: ${OUTPUT}" >> re-runL2.log
-		rm -rf ${OUTPUT}.gfeat
+# --- EDIT HERE end: exceptions and conditionals for the task; need to exclude bad/missing runs
 
-		# ppi has more contrasts than act (phys), so need a different L2 template
-		if [ "${type}" == "act" ]; then
-			ITEMPLATE=${maindir}/templates/L2_task-trust_model-01_type-act_nruns-${nruns}.fsf
-			NCOPES=18
-		else
-			ITEMPLATE=${maindir}/templates/L2_task-trust_model-01_type-ppi_nruns-${nruns}.fsf
-			NCOPES=19
-		fi
 
-		# set output template and run template-specific analyses
-		OTEMPLATE=${MAINOUTPUT}/L2_task-trust_model-01_type-${type}.fsf
-		if [ ${nruns} -eq 5 ]; then
-			sed -e 's@OUTPUT@'$OUTPUT'@g' \
-			-e 's@INPUT1@'$INPUT1'@g' \
-			-e 's@INPUT2@'$INPUT2'@g' \
-			-e 's@INPUT3@'$INPUT3'@g' \
-			-e 's@INPUT4@'$INPUT4'@g' \
-			-e 's@INPUT5@'$INPUT5'@g' \
-			<$ITEMPLATE> $OTEMPLATE
-		elif [ ${nruns} -eq 4 ]; then
-			sed -e 's@OUTPUT@'$OUTPUT'@g' \
-			-e 's@INPUT1@'$INPUT1'@g' \
-			-e 's@INPUT2@'$INPUT2'@g' \
-			-e 's@INPUT3@'$INPUT3'@g' \
-			-e 's@INPUT4@'$INPUT4'@g' \
-			<$ITEMPLATE> $OTEMPLATE
-		elif [ ${nruns} -eq 3 ]; then
-			sed -e 's@OUTPUT@'$OUTPUT'@g' \
-			-e 's@INPUT1@'$INPUT1'@g' \
-			-e 's@INPUT2@'$INPUT2'@g' \
-			-e 's@INPUT3@'$INPUT3'@g' \
-			<$ITEMPLATE> $OTEMPLATE
-		elif [ ${nruns} -eq 2 ]; then
-			sed -e 's@OUTPUT@'$OUTPUT'@g' \
-			-e 's@INPUT1@'$INPUT1'@g' \
-			-e 's@INPUT2@'$INPUT2'@g' \
-			<$ITEMPLATE> $OTEMPLATE
-		fi
-		feat $OTEMPLATE
+# check for existing output and re-do if missing/incomplete
+OUTPUT=${MAINOUTPUT}/L2_task-${task}_model-02_type-${type}_sm-${sm}
+if [ -e ${OUTPUT}.gfeat/cope${NCOPES}.feat/cluster_mask_zstat1.nii.gz ]; then # check last (act) or penultimate (ppi) cope
+	echo "skipping existing output"
+else
+	echo "re-doing: ${OUTPUT}" >> re-runL2.log
+	rm -rf ${OUTPUT}.gfeat
 
-		# delete unused files
-		for cope in `seq ${NCOPES}`; do
-			rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/res4d.nii.gz
-			rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/corrections.nii.gz
-			rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/threshac1.nii.gz
-			rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/filtered_func_data.nii.gz
-			rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/var_filtered_func_data.nii.gz
-		done
+	# set output template and run template-specific analyses
+	OTEMPLATE=${MAINOUTPUT}/L2_task-${task}_model-02_type-${type}.fsf
+	sed -e 's@OUTPUT@'$OUTPUT'@g' \
+	-e 's@INPUT1@'$INPUT1'@g' \
+	-e 's@INPUT2@'$INPUT2'@g' \
+	feat $OTEMPLATE
 
-	fi
+	# delete unused files
+	for cope in `seq ${NCOPES}`; do
+		rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/res4d.nii.gz
+		rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/corrections.nii.gz
+		rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/threshac1.nii.gz
+		rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/filtered_func_data.nii.gz
+		rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/var_filtered_func_data.nii.gz
+	done
+
 fi
