87dd1a825251f5723b81ad421803a74672f02dbc
David V. Smith <david.v.smith@temple.edu>
2021-08-13 00:16:09 -0400

    rewrite for this repo

--- code/L3stats.sh
+++ code/L3stats.sh
@@ -18,13 +18,13 @@ scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
 
 # study-specific inputs and general output folder
-task=trust
-N=48
+task=ultimatum
+N=47
 copenum=$1
-copenum_thresh_randomise=10 # actual contrasts start with cope10 (rec > def). no need to do randomise main effects (e.g., rec > nothing/fixation/baseline)
+copenum_thresh_randomise=7 # actual contrasts start here. no need to do randomise main effects (e.g., reward > nothing/fixation/baseline)
 copename=$2
 REPLACEME=$3 # this defines the parts of the path that differ across analyses
-MAINOUTPUT=${maindir}/derivatives/fsl/L3_model-01_task-${task}_n${N}_flame1+2_retest
+MAINOUTPUT=${maindir}/derivatives/fsl/L3_model-02_task-${task}_n${N}_flame1+2_retest
 mkdir -p $MAINOUTPUT
 
 
--- code/run_L3stats.sh
+++ code/run_L3stats.sh
@@ -10,26 +10,26 @@ maindir="$(dirname "$scriptdir")"
 
 
 # this loop defines the different types of analyses that will go into the group comparisons
-for analysis in act nppi-dmn nppi-ecn ppi_seed-PCC ppi_seed-rFFA ppi_seed-rTPJ ppi_seed-VMPFCface ppi_seed-VMPFCwin ppi_seed-VS; do
+for analysis in act nppi-dmn nppi-ecn; do
 	analysistype=type-${analysis}
 
 	# these define the cope number (copenum) and cope name (copename)
-	#for copeinfo in "1 c_C" "2 c_F" "3 c_S" "4 C_def" "5 C_rec" "6 F_def" "7 F_rec" "8 S_def" "9 S_rec" "10 rec-def" "11 face" "12 rec-def_F-S" "13 F-S" "14 F-C" "15 S-C" "16 rec_SocClose" "17 def_SocClose" "18 rec-def_SocClose" "19 phys"; do
-	for copeinfo in "10 rec-def" "12 rec-def_F-S" "16 rec_SocClose" "17 def_SocClose" "18 rec-def_SocClose"; do	
-	#for copeinfo in "4 C_def" "5 C_rec" "6 F_def" "7 F_rec" "8 S_def" "9 S_rec"; do
+	# "1 comp" "2 comp_p" "3 in" "4 in_p" "5 out" "6 out_p"
+	# "7 in_p-out_p" "8 soc_p-nonsoc_p" "9 in-out" "10 soc-nonsoc" "11 phys"
+	for subrun in "1 comp" "2 comp_p" "3 in" "4 in_p" "5 out" "6 out_p" "7 in_p-out_p" "8 soc_p-nonsoc_p" "9 in-out" "10 soc-nonsoc" "11 phys"; do
 
 		# split copeinfo variable
 		set -- $copeinfo
 		copenum=$1
 		copename=$2
 
-		# skip non-existent contrast for activation analysis
-		if [ "${analysistype}" == "type-act" ] && [ "${copeinfo}" == "19 phys" ]; then
+		if [ "${analysistype}" == "type-act" ] && [ "${copeinfo}" == "11 phys" ]; then
 			echo "skipping phys for activation since it does not exist..."
 			continue
 		fi
 
-		NCORES=12 
+
+		NCORES=9
 		SCRIPTNAME=${maindir}/code/L3stats.sh
 		while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
 			sleep 1s
