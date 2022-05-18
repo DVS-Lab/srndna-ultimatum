106318e83a1cfa87a32cafca2b65c85d22be43e4
DVSneuro <david.v.smith@temple.edu>
2022-05-18 18:31:37 -0400

    change output naming, fix DVARS, add correlation, and clean up intermediate output

--- code/mssddvarscalc.sh
+++ code/mssddvarscalc.sh
@@ -4,28 +4,38 @@
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
 datadir=/data/projects/srndna-data
-outdir=${maindir}/derivatives/fsl/sub-{$sub}
 
 # study-specific inputs
 sub=$1
 run=$2
 sm=6
 TASK=ultimatum
-mask=${outdir}/L1_task-${TASK}_model-02_type-act_run-0${run}_sm-${sm}.feat/mask
+
+# input/output paths
+outdir=${maindir}/derivatives/fsl/sub-${sub}
+mask=${outdir}/L1_task-${TASK}_model-02_type-act_run-0${run}_sm-${sm}.feat/mask # may be better to use the fmriprep mask?
 mcf=${datadir}/derivatives/fmriprep/sub-${sub}/func/sub-${sub}_task-${TASK}_run-${run}_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz
 
 # define the number of volumes, and minus one to do the math
-tmax=`$FSLDIR/bin/fslval ${mcf} dim4`;
+tmax=`fslval ${mcf} dim4`;
 tmax1=`echo $tmax - 1 | bc`;
 
 # set up the motion-corrected files
-$FSLDIR/bin/fslroi $mcf ${mcf}1 0 $tmax1
-    $FSLDIR/bin/fslroi $mcf ${mcf}2 1 $tmax1
-    $FSLDIR/bin/fslmaths 
+fslroi $mcf ${mcf}1 0 $tmax1
+fslroi $mcf ${mcf}2 1 $tmax1
 
 # generate MSSD (as per Nomi et al., 2017: https://www.jneurosci.org/content/jneuro/37/22/5539.full.pdf)
-# MSSD: #  
-$FSLDIR/bin/fslmaths ${mcf}2 -sub ${mcf}1 -mas ${mask} -sqr -Tmean ${outdir}_mc/mssd_3d -odt float
+fslmaths ${mcf}2 -sub ${mcf}1 -mas ${mask} -sqr -Tmean ${outdir}/sub-${sub}_run-${run}_mssd3d -odt float
 
-# for comparison DVARS (from fsl_motion_outliers): # 
-$FSLDIR/bin/fslmaths ${mcf}2 -sub ${mcf}1 -mas ${mask} -sqr -Xmean -Ymean -Zmean -div $maskmean $sqrtcom ${outdir}_mc/dvars_4d -odt float
+# for comparison DVARS (from fsl_motion_outliers): #
+maskmean=`fslstats ${mask} -m`;
+brainmed=`fslstats ${mcf} -k ${mask} -P 50`;
+fslmaths ${mcf}2 -sub ${mcf}1 -mas ${mask} -sqr -Xmean -Ymean -Zmean -div $maskmean -sqrt ${outdir}/sub-${sub}_run-${run}_dvars4d -odt float
+fslmaths ${outdir}/sub-${sub}_run-${run}_dvars4d -div $brainmed -mul 1000 ${outdir}/sub-${sub}_run-${run}_dvars4d
+fslmaths sub-${sub}_run-${run}_dvars4d -Tmean sub-${sub}_run-${run}_dvarsTmean
+
+# correlate MSSD and DVARS (will want to output this to a file)
+fslcc -t -1 --noabs -m $mask sub-${sub}_run-${run}_dvars4d -Tmean sub-${sub}_run-${run}_dvarsTmean ${outdir}/sub-${sub}_run-${run}_mssd3d
+
+# clean up intermediate files
+rm -rf ${mcf}1.nii.gz ${mcf}2.nii.gz
