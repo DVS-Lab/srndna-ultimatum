be0f67179cfc9156b10b0b8b4d4a4484f74c458d
DVSneuro <david.v.smith@temple.edu>
2022-05-19 21:47:19 -0400

    add option for run number

--- code/wbMSSDandDVARS.sh
+++ code/wbMSSDandDVARS.sh
@@ -7,11 +7,13 @@
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
 
+run=$1
+
 # study-specific inputs and outputs
-input=${maindir}/derivatives/fsl/mergedAllSubsRuns_mssd.nii.gz
-output=${maindir}/derivatives/randomise_mssd_testing
-design=${maindir}/derivatives/design_n100_testing.mat
-contrasts=${maindir}/derivatives/design_n100_testing.con
+input=${maindir}/derivatives/fsl/mergedAllSubs_run-${run}_mssd.nii.gz
+output=${maindir}/derivatives/randomise_mssd_run-${run}_testing
+design=${maindir}/derivatives/design_n50_run-${run}_testing.mat
+contrasts=${maindir}/derivatives/design_n50_run-${run}_testing.con
 mask=${maindir}/derivatives/fsl/mask.nii.gz
 
 randomise -i $input -o $output -d $design -t $contrasts -m $mask -T -c 3.1 -n 10000
