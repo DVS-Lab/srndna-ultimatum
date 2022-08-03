f1785ac9d040ab9028a3151c90a1f59c4f1d6c0b
DVSneuro <david.v.smith@temple.edu>
2022-08-03 12:42:49 -0400

    fix paths and subject loop

--- code/extractSingleTrialEstimates.sh
+++ code/extractSingleTrialEstimates.sh
@@ -17,8 +17,6 @@ for ppi in 0; do # putting 0 first will indicate "activation"
 
 	# loops through the subject/run list
 	for sub in 104 105 106 107 108 109 110 111 112 113 115 116 117 118 120 121 122 124 125 126 127 128 129 130 131 132 133 134 135 136 137 138 140 141 142 143 144 145 147 149 150 151 152 153 154 155 156 157 158 159; do
-		set -- ${subrun}
-		sub=$1
 		echo "running: sub-${sub} on conn-${ppi} at `date`..."
 
 
@@ -26,17 +24,17 @@ for ppi in 0; do # putting 0 first will indicate "activation"
 
 			# common directory for zstat outputs
 			MAINOUTPUT=${maindir}/derivatives/fsl/sub-${sub}
-			zoutdir=${MAINOUTPUT}/LSS-images_task-${TASK}_model-02_conn-${ppi}_run-0${run}_sm-${sm}
+			zoutdir=${MAINOUTPUT}/LSS-images_task-${TASK}_model-01_type-act_run-0${run}
 			cd $zoutdir
 			rm -rf sub-${sub}*.nii.gz
-			fslmerge -t sub-${sub}_run-0${run}_conn-${ppi}_merged_z zstat_trial-*.nii.gz
+			fslmerge -t sub-${sub}_run-0${run}_type-act_merged_z zstat_trial-*.nii.gz
 
 			# output for extractions
 			out_meants=${maindir}/derivatives/singletrial/sub-${sub}
 			mkdir -p ${out_meants}
 
 			maskfile=${maindir}/masks/${mask}.nii.gz
-			fslmeants -i ${zoutdir}/sub-${sub}_run-0${run}_conn-${ppi}_merged_z.nii.gz \
+			fslmeants -i ${zoutdir}/sub-${sub}_run-0${run}_type-act_merged_z.nii.gz \
 				-o ${out_meants}/sub-${sub}_run-0${run}_mask-${mask}.txt \
 				-m ${maskfile}
 
